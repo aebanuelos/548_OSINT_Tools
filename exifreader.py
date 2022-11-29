b05f8f63c990b1a6f965c14e7bbb52c5af49b918
aebanuelos <43016014+aebanuelos@users.noreply.github.com>
2022-11-29 14:12:30 -0600

    Update exifreader.py

--- exifreader.py
+++ exifreader.py
@@ -1,6 +1,7 @@
-from PIL import Image # pip install Pillow
-from PIL.ExifTags import TAGS # ^^^
+from PIL import Image, ExifTags  # pip install Pillow
+from PIL.ExifTags import TAGS  # ^^^
 import os
+
 
 def extractData(image, imagename):
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
@@ -22,7 +23,6 @@ def extractData(image, imagename):
         "Frames in Image": getattr(imageOpen, "n_frames", 1)
     }
 
-
     for label, value in info_dict.items():
         print(f"{label:25}: {value}")
         f.write(f"{label:25}: {value}\n")
@@ -38,5 +38,37 @@ def extractData(image, imagename):
             data = data.decode()
         print(f"{tag:25}: {data}")
         f.write(f"{tag:25}: {data}\n")
+
+    def get_gps_exif(image):
+        data = dict()
+        exif = image.getexif()
+        if exif:
+            # Basic exif (camera make/model, etc)
+            for key, val in exif.items():
+                data[ExifTags.TAGS[key]] = val
+
+            # Aperture, shutter, flash, lens, tz offset, etc
+            ifd = exif.get_ifd(0x8769)
+            for key, val in ifd.items():
+                data[ExifTags.TAGS[key]] = val
+
+            # GPS Info
+            ifd = exif.get_ifd(0x8825)
+            for key, val in ifd.items():
+                data[ExifTags.GPSTAGS[key]] = val
+
+        filteredKeys = ['GPSLatitudeRef', 'GPSLatitude', 'GPSLongitudeRef', 'GPSLongitude']
+
+        gps_dict = {}
+        for item in data:
+            if item in filteredKeys:
+                gps_dict[f"{item}"] = data[f"{item}"]
+        result = str(gps_dict)
+        return result
+
+    f.write("\nIF ANY GPS DATA IN DMS (degrees, minutes, seconds):\n")
+    f.write(get_gps_exif(imageOpen))
+    print("\nIF ANY GPS DATA IN DMS (degrees, minutes, seconds):\n")
+    print(get_gps_exif(imageOpen))
 
     f.close()
