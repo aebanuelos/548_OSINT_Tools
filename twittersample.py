443f8f128f254c497c7cb16da407d573139b5099
aebanuelos <43016014+aebanuelos@users.noreply.github.com>
2022-11-28 23:08:19 -0600

    Updating Twitter to save file and hiding keys :)

--- twittersample.py
+++ twittersample.py
@@ -1,4 +1,5 @@
-import tweepy as tweepy
+import tweepy as tweepy  # pip install tweepy
+import os
 import pandas as pd
 from tabulate import tabulate
 from tweepy import OAuthHandler
@@ -6,15 +7,15 @@ from tweepy import OAuthHandler
 ########################################################################################################################
 # Verification Keys
 
-# API keys that you saved earlier
-api_key = "vjUxsGbLepiUmJVLC3b4tcOiH"
-api_secrets = "r5GauosrDEgePMo8arG5MHPmj1AKigzEXTpo6MFbmQc8vyZasD"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAABUljQEAAAAAh43j7j58%2BF%2F2pIQ3t3CxGHq35Ds%3DRBDmN3jywb8MfGW4LzQOpLSd2kJmxkgqa0iXvZOewBp7xeusao"
-access_token = "1591932689698930689-4MrsgOFsDdFXuHQyVH0TOUpWvtELwT"
-access_secret = "yovRN3rprloNTijJE5pcY2W96MmJk6OKRvA7tIHe0zGXN"
+# API keys that match to the settings of your twitter dev account
+api_key = "***"
+api_secrets = "***"
+bearer_token = "***"
+access_token = "***"
+access_secret = "***"
 
-client_id = "ZHloa2wzSk5RTkJMR0JLVk5DRmk6MTpjaQ"
-client_secret = "M9JgQo4ptTWGKaG16rNt_uVUz_FK2amfzmeZ03h-HgxJavkfqj"
+client_id = "***"
+client_secret = "***"
 
 # goated Client documentation: https://docs.tweepy.org/en/stable/client.html#
 
@@ -29,7 +30,13 @@ answer = Client.get_home_timeline()
 
 def get_users_tweets(inputUsername):
     try:
-        #username_input = input("Enter Username for User's Timeline: ")
+        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        newFileName = f"{inputUsername}_Twitter_Connections.txt"
+        filepath = os.path.join(desktop, newFileName)
+        if not os.path.exists(desktop):
+            os.makedirs(desktop)
+        f = open(filepath, "a")
+        # username_input = input("Enter Username for User's Timeline: ")
         users_id = Client.get_user(username=inputUsername)
         users_tweets = Client.get_users_tweets(id=users_id['data']['id'])
         recent_mentions = set()
@@ -37,11 +44,11 @@ def get_users_tweets(inputUsername):
         for x in range(num_of_mentions):
             if "@" in users_tweets['data'][x]['text']:
                 username_only = extract_string(users_tweets['data'][x]['text'])
-                #recent_mentions[f'{x}_mention'] = username_only
+                # recent_mentions[f'{x}_mention'] = username_only
                 recent_mentions.add(username_only)
             else:
                 continue
-        updated_mentions = {s for s in recent_mentions if s} # catches error of grabbing NULL user
+        updated_mentions = {s for s in recent_mentions if s}  # catches error of grabbing NULL user
 
         final_mentions = {}
         user_accounts = []
@@ -53,9 +60,21 @@ def get_users_tweets(inputUsername):
         final_mentions["Username"] = user_accounts
         final_mentions["Links"] = link_to_accounts
         print(final_mentions)
-        return(final_mentions)
+
+        # appending accounts to text file on desktop
+        i = 1
+        for users in final_mentions['Username']:
+            f.write(
+                f"{i}: Username: {users} & Link: {final_mentions['Links'][final_mentions['Username'].index(users)]}\n")
+            i += 1
+        f.close()
+
+        return (final_mentions)
+
     except Exception as e:
-        print(f'\nPlease ensure the account exists, it is spelled right, and it has public settings.\nYou entered: "{inputUsername}"\nError: {e}')
+        print(
+            f'\nPlease ensure the account exists, it is spelled right, and it has public settings.\nYou entered: "{inputUsername}"\nError: {e}')
+
 
 ''' 
 Model for how pyqt5 interprets table data
@@ -63,6 +82,7 @@ data = {'col1':['1','2','3','4'],
         'col2':['1','2','1','3'],
         'col3':['1','1','2','1']}
 '''
+
 
 def extract_string(string):
     sub1 = "@"
@@ -73,8 +93,7 @@ def extract_string(string):
     # getting elements in between
     for idx in range(idx1 + len(sub1), idx2):
         res = res + string[idx]
-    return(res)
-
+    return (res)
 
 # df = pd.DataFrame.from_dict(users_tweets)
 # print(tabulate(df, headers='keys', tablefmt='psql'))
